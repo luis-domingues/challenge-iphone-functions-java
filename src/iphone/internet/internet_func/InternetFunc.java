a7033b33ea47f2b722676533d8ee7391218d512d
Luís Paulo Domingues <157630664+luis-domingues@users.noreply.github.com>
2024-08-22 15:19:57 -0300

    feat: add browser methods

--- src/iphone/internet/internet_func/InternetFunc.java
+++ src/iphone/internet/internet_func/InternetFunc.java
@@ -1,2 +1,21 @@
-package iphone.internet.internet_func;public class InternetFunc {
+package iphone.internet.internet_func;
+
+import iphone.internet.Browser;
+
+public class InternetFunc implements Browser {
+    @Override
+    public String displayPage (String url) {
+        System.out.println (url);
+        return url;
+    }
+
+    @Override
+    public void newTab () {
+        System.out.println ("Adding new tab");
+    }
+
+    @Override
+    public void reloadPage () {
+        System.out.println ("Reloading page");
+    }
 }
