aff130cddb956343eadb1bcbbf00464da7325a04
Luís Paulo Domingues <157630664+luis-domingues@users.noreply.github.com>
2024-08-22 15:22:54 -0300

    feat: add overridden methods from spotify interface

--- src/iphone/music/music_func/MusicFunc.java
+++ src/iphone/music/music_func/MusicFunc.java
@@ -1,2 +1,21 @@
-package iphone.music.music_func;public class MusicFunc {
+package iphone.music.music_func;
+
+import iphone.music.Spotify;
+
+public class MusicFunc implements Spotify {
+    @Override
+    public void play () {
+        System.out.println ("The app is playing music");
+    }
+
+    @Override
+    public void pause () {
+        System.out.println ("The app paused music");
+    }
+
+    @Override
+    public String selectSong (String music) {
+        System.out.println (music);
+        return music;
+    }
 }
