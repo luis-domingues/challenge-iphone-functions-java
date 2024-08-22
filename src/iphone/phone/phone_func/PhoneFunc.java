66a931d7a88bf752ecdce7f88f1a0bce2b15478f
Luís Paulo Domingues <157630664+luis-domingues@users.noreply.github.com>
2024-08-22 15:24:35 -0300

    feat: add overridden methods from phone interface

--- src/iphone/phone/phone_func/PhoneFunc.java
+++ src/iphone/phone/phone_func/PhoneFunc.java
@@ -1,2 +1,21 @@
-package iphone.phone.phone_func;public class PhoneFunc {
+package iphone.phone.phone_func;
+
+import iphone.phone.Phone;
+
+public class PhoneFunc implements Phone {
+    @Override
+    public String call (String number) {
+        System.out.println (number);
+        return number;
+    }
+
+    @Override
+    public void answerThePhone () {
+        System.out.println ("Answer the call");
+    }
+
+    @Override
+    public void voiceMail () {
+        System.out.println ("Answer Voice Mail");
+    }
 }
